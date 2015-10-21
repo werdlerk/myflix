6a595fc1f3698422aa8b1a097c1c950a00e5ecb1
Koen Werdler <koen@morgenwerk.nl>
2015-10-21 23:03:16 +0200

    Changed SMTP settings for better readability and set authentication method as a string, according to the (Action Mailer Basics) guide.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,12 +20,12 @@ Myflix::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'myflix.codefish.org' }
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: :login,
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'login',
+    enable_starttls_auto: true
   }
 end
