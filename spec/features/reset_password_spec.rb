535b226ba35ea649f69a647bcc871891d87cb222
Koen Werdler <koen@morgenwerk.nl>
2016-01-05 21:25:37 +0100

    Integration test for resetting password

---
+++ spec/features/reset_password_spec.rb
@@ -0,0 +1,47 @@
+require "spec_helper"
+
+feature 'Reset password' do
+  given(:user) { Fabricate(:user, email: "john@example.com", password: "Password", name: "John Doe") }
+
+  background  do
+    clear_emails
+  end
+
+  context 'email unknown' do
+
+    scenario 'unable to reset password' do
+      visit forgot_password_path
+
+      fill_in "Email Address", with: 'bob@example.com'
+      click_button "Send Email"
+
+      expect(page).to have_content "The given e-mail address can't be found."
+    end
+  end
+
+  scenario 'reset password using the link in the email' do
+    visit forgot_password_path
+    fill_in "Email Address", with: user.email
+    click_button "Send Email"
+    expect(page).to have_content "We have send an email with instructions to reset your password."
+
+    open_email user.email
+    expect(current_email).to have_content "Please click on the link below to set a new password."
+
+    current_email.click_link 'Reset password'
+    expect(page).to have_content "Reset Your Password"
+
+    fill_in "New Password", with: 'Boobies'
+    click_button "Reset Password"
+
+    expect(page).to have_content "Your password has been changed."
+    expect(page).to have_content "Sign in"
+
+    fill_in "Email Address", with: user.email
+    fill_in "Password", with: "Boobies"
+    click_button "Sign in"
+
+    expect(page).to have_content "Welcome back, #{user.name}"
+  end
+
+end
