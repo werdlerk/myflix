b4bd34468eb05fc9b1a2fbba285fb186e1632abe
Koen Werdler <koen@morgenwerk.nl>
2016-01-20 22:16:22 +0100

    Added missing require for basic authentication settings for Sidekiq Web UI

--- config/initializers/sidekiq_web.rb
+++ config/initializers/sidekiq_web.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-  username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
-end if Rails.env.production?
+  username == ( ENV["SIDEKIQ_USERNAME"] || "admin" ) && password == ( ENV["SIDEKIQ_PASSWORD"] || "sidekiq" )
+end
