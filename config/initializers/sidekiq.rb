a387ee1479d9a5c5b392f759c45fcdfed6a4cd62
Koen Werdler <koen@morgenwerk.nl>
2016-01-20 22:46:45 +0100

    Set the number of connections for Sidekiq to fix Heroku from maxing out the number of clients to Redis.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,9 @@
+require 'sidekiq'
 require 'sidekiq/web'
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
 
 Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   username == ( ENV["SIDEKIQ_USERNAME"] || "admin" ) && password == ( ENV["SIDEKIQ_PASSWORD"] || "sidekiq" )
