ba0960f155d3f6dfd8b29f306a3003759bd75014
Koen Werdler <koen@morgenwerk.nl>
2016-02-02 21:02:09 +0100

    Use the SENTRY_DSN environment variable instead of putting the DSN value in clear code.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 Raven.configure do |config|
+  config.silence_ready = true
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.dsn = 'https://f29cfb8f265d4427ba9b341c99be4d70:bcc4165f99344ba09dc54588324b7447@app.getsentry.com/64564'
+  config.dsn = ENV['SENTRY_DSN'] if ENV['SENTRY_DSN']
   config.environments = ['staging', 'production']
 end
