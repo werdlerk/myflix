54a06ea41debb0b932ef5f4f429bb5ec08a305d5
Koen Werdler <koen@morgenwerk.nl>
2016-02-04 23:30:55 +0100

    Added creating the Video in the Admin::VideosController

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -2,6 +2,25 @@ class Admin::VideosController < AdminController
 
   def new
     @video = Video.new
+    @categories = Category.all
+  end
+
+  def create
+    @video = Video.new(video_params)
+
+    if @video.save
+      flash[:success] = "Video created"
+      redirect_to admin_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :description, :category_id)
   end
 
 end
--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -21,6 +21,62 @@ describe Admin::VideosController do
         expect(assigns(:video)).to be_a_new Video
       end
     end
+  end
+
+  describe 'POST #create' do
+    it_behaves_like "requires admin" do
+      let(:action) { post :create, video: Fabricate.attributes_for(:video) }
+    end
+
+    context 'as admin user' do
+      before do
+        set_current_admin
+      end
+
+      context 'valid input' do
+        let(:video_attributes) { Fabricate.attributes_for(:video) }
+
+        it 'saves the Video' do
+          post :create, video: video_attributes
+
+          expect(Video.count).to eq 1
+        end
+
+        it 'redirects to the admin_path' do
+          post :create, video: video_attributes
+
+          expect(response).to redirect_to admin_path
+        end
+
+        it 'sets the flash message' do
+          post :create, video: video_attributes
+
+          expect(flash[:success]).to be_present
+        end
+      end
+
+      context 'invalid input' do
+        let(:video_attributes) { Fabricate.attributes_for(:video, title: nil) }
+
+        it 'doesnt save the Video' do
+          post :create, video: video_attributes
+
+          expect(Video.count).to eq 0
+        end
+
+        it 'renders the new template' do
+          post :create, video: video_attributes
+
+          expect(response).to render_template 'new'
+        end
+
+        it 'sets the @video variable' do
+          post :create, video: video_attributes
+
+          expect(assigns(:video)).to be_present
+        end
+      end
+    end
 
 
   end
