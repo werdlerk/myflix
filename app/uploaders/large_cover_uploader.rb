7113d2fefefd55960750c25dd8b949e92265cb4f
Koen Werdler <koen@morgenwerk.nl>
2016-02-17 21:55:25 +0100

    Remove the store_dir method setting from the Uploaders

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -3,10 +3,6 @@ class LargeCoverUploader < CarrierWave::Uploader::Base
 
   process :resize_to_fill => [665, 375]
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -3,10 +3,6 @@ class SmallCoverUploader < CarrierWave::Uploader::Base
 
   process :resize_to_fill => [166, 236]
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
