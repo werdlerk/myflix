c00a1f18cc987c2362c5c7f5155d46e1bc6acc70
Koen Werdler <koen@morgenwerk.nl>
2016-03-08 21:03:39 +0100

    Changed dynamic finder to find_by method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   before_action :disallow_authenticated_users, only: [:new, :create]
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
