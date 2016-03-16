e6b3a05b26b5290e83bdf5ef75e02cd4fe56d1ce
Koen Werdler <koen@morgenwerk.nl>
2016-03-16 21:03:41 +0100

    Small changes to StripeWrapperSpec

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -13,37 +13,39 @@ describe StripeWrapper, :vcr do
   end
 
   describe StripeWrapper::Charge do
-    let(:charge) { StripeWrapper::Charge.create(token: stripe_token.id, customer: "Klaas Vaak", amount: 300) }
+    let(:charge) { StripeWrapper::Charge.create(token: stripe_token.id, customer: "Klaas Vaak", description: "signup fee", amount: 300) }
 
-    context 'with valid card' do
-      let(:card_number) { '4242424242424242'}
+    describe '.create' do
+      context 'with valid card' do
+        let(:card_number) { '4242424242424242'}
 
-      it 'charges the card succesfully' do
-        expect(charge).to be_succesful
+        it 'charges the card succesfully' do
+          expect(charge).to be_succesful
+        end
+
+        it 'has no error message' do
+          expect(charge.error_message).to be_nil
+        end
+
+        it 'has a customer_id' do
+          expect(charge.customer_id).to be_present
+        end
       end
 
-      it 'has no error message' do
-        expect(charge.error_message).to be_nil
-      end
+      context 'with invalid card' do
+        let(:card_number) { '4000000000000002'}
 
-      it 'has a customer_id' do
-        expect(charge.customer_id).to be_present
-      end
-    end
+        it 'does not charge the card succesfully' do
+          expect(charge).not_to be_succesful
+        end
 
-    context 'with invalid card' do
-      let(:card_number) { '4000000000000002'}
+        it 'contains an error message' do
+          expect(charge.error_message).to eq "Your card was declined."
+        end
 
-      it 'does not charge the card succesfully' do
-        expect(charge).not_to be_succesful
-      end
-
-      it 'contains an error message' do
-        expect(charge.error_message).to eq "Your card was declined."
-      end
-
-      it 'has no customer id' do
-        expect(charge.customer_id).to be_nil
+        it 'has no customer id' do
+          expect(charge.customer_id).to be_nil
+        end
       end
     end
   end
