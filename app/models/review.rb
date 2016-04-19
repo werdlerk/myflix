1ac82ce756e153c2a77b9f1ae790418dbbb54f7a
Koen Werdler <koen@morgenwerk.nl>
2016-04-19 23:12:20 +0200

    No need for complex after_commit hooks to update the Video after saving / updating a review. The ActiveRecord's touch setting will do the same.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,13 +1,9 @@
 class Review < ActiveRecord::Base
-  belongs_to :video
+  belongs_to :video, touch: true
   belongs_to :author, foreign_key: 'user_id', class_name: 'User'
 
   validates_presence_of :video, :author, :rating, :text
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to:5 }
 
   delegate :title, to: :video, prefix: :video
-
-  after_commit on: [:create]  { video.__elasticsearch__.index_document  }
-  after_commit on: [:update]  { video.__elasticsearch__.update_document }
-  after_commit on: [:destroy] { video.__elasticsearch__.delete_document }
 end
