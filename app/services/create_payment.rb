d223e972d86dddd44b0d1ebbe27efc29d909ff85
Koen Werdler <koen@morgenwerk.nl>
2016-04-21 23:05:08 +0200

    Explicit failure if user is not defined instead of not creating the Payment when the user can't be found.

--- app/services/create_payment.rb
+++ app/services/create_payment.rb
@@ -3,7 +3,7 @@ class CreatePayment
   def call(event)
     charge = event.data.object
     user = User.find_by(stripe_customer_id: charge.customer)
-    Payment.create(amount_cents: charge.amount, user: user, reference_id: charge.id) if user
+    Payment.create!(amount_cents: charge.amount, user: user, reference_id: charge.id)
   end
 
 end
